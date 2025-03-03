cafb8b511af4eb4bd077e32888321fa6311a2b00
recep-demir <demir.rp@gmail.com>
2025-03-03 14:48:45 +0100

    add css styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input 
@@ -16,6 +29,19 @@ function App() {
             
           />
         </div>
+
+        <div className="location-box">
+            <div className="location">Luzern</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className="weather-box">
+            <div className="temp">
+              7°c
+            </div>
+            <div className="weather">sunny</div>
+          </div>
+        
         </main>
       
     </div>
