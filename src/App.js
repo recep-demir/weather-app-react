401660bcd60180ba8b36071c00829982a3cc9378
recep-demir <demir.rp@gmail.com>
2025-03-03 21:28:08 +0100

    edit city name

--- src/App.js
+++ src/App.js
@@ -1,64 +1,14 @@
-import { useState } from 'react';
 import './App.css';
-const api = {
-  key: "44502fc60dbbececfb2a2a52f202564e",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+import Home from './pages/Home';
+
 
 function App() {
-  const [query,setQuery] = useState("")
-  const [weather,setWeather] = useState({})
-
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
-  }
-
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-  }
+  
 
   return (
     <div className="app">
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            
-          />
-        </div>
+      <Home/>
 
-        <div className="location-box">
-            <div className="location">Luzern</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-
-          <div className="weather-box">
-            <div className="temp">
-              7°c
-            </div>
-            <div className="weather">sunny</div>
-          </div>
-        
-        </main>
       
     </div>
   );
